63deb911bd229283f7e3ffb9839d230e4e7b8cc8
LuanCarvalho0 <luan_carvalho1997@hotmail.com>
2022-06-22 12:16:20 -0300

    kk

--- config.py
+++ config.py
@@ -11,5 +11,4 @@ SQLALCHEMY_DATABASE_URI = \
         servidor = 'us-cdbr-east-05.cleardb.net',
         database = 'heroku_b8bfc4054fcd6c0'
     )
-#mysql://:@/?reconnect=true
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
