83eacb21176b13e5fe3e0ef539b4c638f373f7e0
LuanCarvalho0 <luan_carvalho1997@hotmail.com>
2022-06-22 12:12:28 -0300

    adicionando BD

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ SECRET_KEY = 'luan'
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD = 'mysql+mysqlconnector',
-        usuario = 'root',
-        senha = 'Carvalho19',
-        servidor = 'localhost',
-        database = 'jogoteca'
+        usuario = 'b2259be377ae62',
+        senha = '3803dd2f',
+        servidor = 'us-cdbr-east-05.cleardb.net',
+        database = 'heroku_b8bfc4054fcd6c0'
     )
-
+#mysql://:@/?reconnect=true
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
